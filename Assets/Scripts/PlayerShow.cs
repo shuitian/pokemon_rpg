92b31ff5856348a07628ba2f32313199c1b18d31
shuitian <a8998682@163.com>
2016-01-18 17:28:27 +0800

    fix last update

---
+++ Assets/Scripts/PlayerShow.cs
@@ -0,0 +1,39 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.UI;
+
+public class PlayerShow : MonoBehaviour {
+
+    public Text nameText;
+    public Text hpText;
+    public Text attackText;
+    public Text defenceText;
+    
+    void UpdateImformation(Player player)
+    {
+        if (player)
+        {
+            if (nameText)
+            {
+                nameText.text = player.playerName;
+            }
+            if (hpText)
+            {
+                hpText.text = player.hp + "/" + player.maxHp;
+            }
+            if (attackText)
+            {
+                attackText.text = "" + player.attack;
+            }
+            if (defenceText)
+            {
+                defenceText.text = player.defence * 100 + "%";
+            }
+        }
+    }
+
+    void Update()
+    {
+        UpdateImformation(Player.Instance());
+    }
+}
